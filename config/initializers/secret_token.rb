cae3b58624b1aaa55f7db5a4ed2f897ae8cedaae
macosxgeek <zbyszek.sobczynski@me.com>
2011-08-02 03:08:31 +0200

    Re-Test

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RubyOnRailsWebCMSBETATEST::Application.config.secret_token = '4fb07170bf6c4163f7775a350520c5e905b1b689be272b6393770b431cf3498da0cd70f30638bceb28d1d659a8b58bf85e40a59f939b2f7f49b556f8b1e506eb'
